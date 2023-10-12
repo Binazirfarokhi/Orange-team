4d1a7e8209901dadaa0a2047c526e429c42c018e
Lemon03 <119447795+Lemon03@users.noreply.github.com>
2023-10-11 21:06:38 -0700

    feat:OT60 User login(front-end)

--- OrangeProject/App.js
+++ OrangeProject/App.js
@@ -3,18 +3,21 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MyTabs from './components/mytabs-component';
 import LoginScreen from './layouts/login.layout';
+import { NativeBaseProvider } from 'native-base';
 
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown:false}}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="MyTabs" component={MyTabs} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown:false}}>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="MyTabs" component={MyTabs} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
 
