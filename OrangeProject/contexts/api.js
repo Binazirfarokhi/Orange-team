c6639d01d8931eab4ed4ddef5431cee37cd9e7c6
hmoonthet <hmoonthet@gmail.com>
2023-10-20 11:58:35 -0700

    logi role updated

--- OrangeProject/contexts/api.js
+++ OrangeProject/contexts/api.js
@@ -1,30 +1,22 @@
 import axios from "axios";
 
 const config = {
-    url: 'http://10.0.0.14:3000'
-}
+  url: "http://10.128.239.77:3000",
+};
 
-const api = ()=> {
+const api = () => {};
 
-}
-
-const get = async(url) => {
-    return await axios.get(`${config.url}${url}`);
-}
-const deleteCall = async(url) => {
-    return await axios.delete(`${config.url}${url}`);
-}
+const get = async (url) => {
+  return await axios.get(`${config.url}${url}`);
+};
+const deleteCall = async (url) => {
+  return await axios.delete(`${config.url}${url}`);
+};
 const post = async (url, body) => {
-    return await axios.post(`${config.url}${url}`, body);
-}
+  return await axios.post(`${config.url}${url}`, body);
+};
 const put = async (url, body) => {
-    return await axios.put(`${config.url}${url}`, body);
-}
+  return await axios.put(`${config.url}${url}`, body);
+};
 
-
-export {
-    get,
-    post,
-    put,
-    deleteCall
-}
+export { get, post, put, deleteCall };
