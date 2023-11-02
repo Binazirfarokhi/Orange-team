672d18e50edad8afabc34bc1c382c8731e01b032
Binazir <104298006+Binazirfarokhi@users.noreply.github.com>
2023-11-02 10:44:05 -0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -5,15 +5,16 @@ const bodyParser = require("body-parser");
 const CORS = require("cors");
 var jsonParser = bodyParser.json();
 
+
 // var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(jsonParser);
 app.use(CORS());
-app.get('/', (req, res) => {res.send('abcdfa')})
 app.use('/profile', require('./controllers/profile.controller'))
 app.use('/auth', require('./controllers/auth.controller'))
 app.use('/children', require('./controllers/children.controller'))
 app.use('/orgs', require('./controllers/orgs.controller'))
 app.use('/chat', require('./controllers/chat.controller'))
+app.use('/photos', require('./controllers/photos.controller'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
