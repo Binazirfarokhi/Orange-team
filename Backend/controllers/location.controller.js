64626762045924f74a61f5c27ffbfa47b6c4fbe0
Binazir <104298006+Binazirfarokhi@users.noreply.github.com>
2023-11-06 22:21:34 -0800

    Update location.controller.js

--- Backend/controllers/location.controller.js
+++ Backend/controllers/location.controller.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const location = require('../services/location.service')
 
 router.get('/', location.getLocation);
 router.get('/displaymap', location.displayMap);
