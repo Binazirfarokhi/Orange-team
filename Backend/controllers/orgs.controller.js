e9a0264f4446e1671d977acbfcc06dacc562fea3
Zaw Htun <104298006+Binazirfarokhi@users.noreply.github.com>
2023-11-08 16:38:27 -0800

    partial update

--- Backend/controllers/orgs.controller.js
+++ Backend/controllers/orgs.controller.js
@@ -1,34 +1,23 @@
-const express = require("express");
-const {
-  getVolunteerByOrgId,
-  org,
-  orgs,
-  saveNewOrg,
-  saveEvent,
-  getEvent,
-  getEvents,
-  getJoinedEvents,
-  deleteEvent,
-  joinEvent,
-  getOrgsByVolunteerId,
-} = require("../services/orgs.service");
-const { setVolunteerPosition } = require("../services/profile.service");
-const router = express.Router();
+const express = require('express')
+const { getVolunteerByOrgId, org, orgs, saveNewOrg, saveEvent, getEvent, getEvents, getJoinedEvents, deleteEvent, joinEvent, getOrgsByVolunteerId } = require('../services/orgs.service')
+const { setVolunteerPosition } = require('../services/profile.service')
+const router = express.Router()
 
 // define the home page route
-router.get("/", orgs);
-router.post("/", saveNewOrg);
-router.post("/event", saveEvent);
-router.post("/event/:userId/:id/:role", joinEvent);
-router.get("/events/:id", getEvents);
-router.get("/events", getEvents);
-router.get("/eventsJoined/:id", getJoinedEvents);
-router.get("/event/:id", getEvent);
-router.patch("/event/:id", saveEvent);
-router.delete("/event/:id", deleteEvent);
-router.get("/:id", org);
-router.get("/volunteer/:id", getVolunteerByOrgId);
-router.patch("/volunteer/:id", setVolunteerPosition);
-router.get("/orgs/:volunteerId", getOrgsByVolunteerId);
+router.get('/', orgs);
+router.post('/', saveNewOrg);
+router.post('/event', saveEvent);
+router.post('/event/:userId/:id/:role', joinEvent);
+router.get('/events/:id', getEvents);
+router.get('/events', getEvents);
+router.get('/eventsJoined/:id', getJoinedEvents);
+router.get('/event/:id', getEvent);
+router.patch('/event/:id', saveEvent);
+router.delete('/event/:id', deleteEvent);
+router.get('/:id', org);
+router.get('/volunteer/:id', getVolunteerByOrgId);
+router.patch('/volunteer/:id', setVolunteerPosition);
+router.get('/orgs/:volunteerId', getOrgsByVolunteerId);
 
-module.exports = router;
+
+module.exports = router
--- Backend/index.js
+++ Backend/index.js
@@ -5,18 +5,20 @@ const bodyParser = require("body-parser");
 const CORS = require("cors");
 var jsonParser = bodyParser.json();
 
+
 // var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(jsonParser);
 app.use(CORS());
-app.use(jsonParser);
-app.use(CORS());
-app.use("/profile", require("./controllers/profile.controller"));
-app.use("/auth", require("./controllers/auth.controller"));
-app.use("/children", require("./controllers/children.controller"));
-app.use("/orgs", require("./controllers/orgs.controller"));
-app.use("/chat", require("./controllers/chat.controller"));
-app.use("/photos", require("./controllers/photos.controller"));
-app.use("/location", require("./controllers/location.controller"));
+
+app.use('/profile', require('./controllers/profile.controller'))
+app.use('/auth', require('./controllers/auth.controller'))
+app.use('/children', require('./controllers/children.controller'))
+app.use('/orgs', require('./controllers/orgs.controller'))
+app.use('/chat', require('./controllers/chat.controller'))
+app.use('/photos', require('./controllers/photos.controller'))
+app.use('/location', require('./controllers/location.controller'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
