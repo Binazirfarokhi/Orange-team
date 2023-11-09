d6e3d4fd705419be9ab10cd87a0db3dfa011d4eb
Lemon03 <119447795+Lemon03@users.noreply.github.com>
2023-11-09 13:06:30 -0800

    eventitem styling

--- OrangeProject/components/event-item.component.js
+++ OrangeProject/components/event-item.component.js
@@ -49,85 +49,58 @@ export default function EventItem({ navigation, event, orgPos, reload }) {
     navigation.navigate("CreateEvent", { id });
   };
 
-  return (
-    <View style={styles.main}>
-      <View
-        style={{
-          flex: 1,
-          paddingRight: 20,
-          alignItems: "center",
-          justifyContent: "center",
-        }}>
-        <Image
-          style={{
-            width: 140,
-            height: 140,
-            borderRadius: 15,
-          }}
-          source={
-            images && images !== null && images.length > 0
-              ? { uri: images[0] }
-              : require("../assets/unilogo.png")
-          }
-        />
-      </View>
-      <View style={{ flex: 2, display: "flex", flexDirection: "column" }}>
-        <TouchableOpacity style={styles.button} onPress={goto}>
-          <View style={{}}>
-            <Text style={styles.name}>{eventName}</Text>
-          </View>
-          <View style={{ paddingTop: 15, paddingBottom: 16 }}>
-            <Text>
-              {date} {time}
-            </Text>
-            <Text>{location}</Text>
-          </View>
-        </TouchableOpacity>
-        <View style={styles.option}>
-          <View style={{ flexDirection: "column", fex: 2, display: "flex" }}>
-            <Text>{ageGroup} Years</Text>
-            <Text>{participants} Participants</Text>
-          </View>
-          <View
-            style={{
-              flex: 1,
-              flexDirection: "row",
-              justifyContent: "space-around",
-            }}>
-            <Button
-              icon={<FontAwesome name="edit" size={20} color="green" />}
-              onPress={editEvent}
-              type="clear"
-            />
-            <Button
-              icon={<FontAwesome name="trash" size={20} />}
-              onPress={deleteEvent}
-              type="clear"
-            />
-          </View>
+    return (
+        <View style={styles.main}>
+            <View style={{ flex: 1, paddingRight: 20, alignItems: 'center', justifyContent: 'center' }}>
+                <Image
+                    style={{
+                        width: 110,
+                        height: 110,
+                        borderRadius: 15,
+                    }}
+                    source={(images && images !== null && images.length > 0)?{ uri: images[0] }: require('../assets/unilogo.png')} />
+            </View>
+            <View style={{ flex: 2, display: 'flex', flexDirection: 'column' }}>
+                <TouchableOpacity style={styles.button} onPress={goto}>
+                    <View style={{}}>
+                        <Text style={styles.name}>{eventName}</Text>
+                    </View>
+                    <View style={{ paddingTop: 15, paddingBottom: 16, }}>
+                        <Text style={styles.datetime}>{date} {time}</Text>
+                        <Text>{location.split(',')[0]}</Text>
+                    </View>
+                </TouchableOpacity>
+                <View style={styles.option}>
+                    <View style={{ flexDirection: 'column', fex: 2, display: 'flex' }}>
+                        <Text>{ageGroup} Years</Text>
+                        <Text>{participants} Participants</Text>
+                    </View>
+                    <View style={{ flex: 1, flexDirection: "row", justifyContent: 'space-around' }}>
+                        <Button icon={<FontAwesome name='edit' size={20} color="green" />} onPress={editEvent} type="clear" />
+                        <Button icon={<FontAwesome name='trash' size={20} />} onPress={deleteEvent} type="clear" />
+                    </View>
+                </View>
+            </View>
         </View>
-      </View>
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  main: {
-    marginTop: 20,
-    paddingTop: 20,
-    paddingLeft: 10,
-    paddingRightt: 10,
-    paddingBottom: 20,
-    display: "flex",
-    minHeight: 150,
-    flexDirection: "row",
-  },
-  name: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  option: {
-    display: "flex",
-    flexDirection: "row",
-  },
-});
+    main: {
+        marginTop: 10,
+        padding: 10,
+        display: 'flex',
+        minHeight: 150,
+        flexDirection: "row",
+    }, name: {
+        fontSize: 17,
+        fontWeight: 'bold'
+    }, datetime: {
+        fontSize: 14,
+        fontWeight: 'bold'
+    }
+    , option: {
+        display: 'flex',
+        flexDirection: 'row',
+    }
+})
