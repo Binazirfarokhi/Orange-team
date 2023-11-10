7a8fad5328779845385f2de0e2d1e3287a8cb6a2
Lemon03 <119447795+Lemon03@users.noreply.github.com>
2023-11-09 23:14:35 -0800

    Edit event bug

--- OrangeProject/components/event-item.component.js
+++ OrangeProject/components/event-item.component.js
@@ -49,40 +49,44 @@ export default function EventItem({ navigation, event, orgPos, reload }) {
     navigation.navigate("CreateEvent", { id });
   };
 
-    return (
-        <View style={styles.main}>
-            <View style={{ flex: 1, paddingRight: 20, alignItems: 'center', justifyContent: 'center' }}>
-                <Image
-                    style={{
-                        width: 110,
-                        height: 110,
-                        borderRadius: 15,
-                    }}
-                    source={(images && images !== null && images.length > 0)?{ uri: images[0] }: require('../assets/unilogo.png')} />
-            </View>
-            <View style={{ flex: 2, display: 'flex', flexDirection: 'column' }}>
-                <TouchableOpacity style={styles.button} onPress={goto}>
-                    <View style={{}}>
-                        <Text style={styles.name}>{eventName}</Text>
-                    </View>
-                    <View style={{ paddingTop: 15, paddingBottom: 16, }}>
-                        <Text style={styles.datetime}>{date} {time}</Text>
-                        <Text>{location.split(',')[0]}</Text>
-                    </View>
-                </TouchableOpacity>
-                <View style={styles.option}>
-                    <View style={{ flexDirection: 'column', fex: 2, display: 'flex' }}>
-                        <Text>{ageGroup} Years</Text>
-                        <Text>{participants} Participants</Text>
-                    </View>
-                    <View style={{ flex: 1, flexDirection: "row", justifyContent: 'space-around' }}>
-                        <Button icon={<FontAwesome name='edit' size={20} color="green" />} onPress={editEvent} type="clear" />
-                        <Button icon={<FontAwesome name='trash' size={20} />} onPress={deleteEvent} type="clear" />
-                    </View>
-                </View>
-            </View>
-        </View>
-    );
+  const eventDate = new Date(date);
+  const dateOptions = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+  const formattedDate = new Intl.DateTimeFormat('en-US', dateOptions).format(eventDate);
+  const displayDateTime = `${formattedDate} ${time}`;
+  return (
+      <View style={styles.main}>
+          <View style={{ flex: 1, paddingRight: 20, alignItems: 'center', justifyContent: 'center' }}>
+              <Image
+                  style={{
+                      width: 110,
+                      height: 110,
+                      borderRadius: 15,
+                  }}
+                  source={(images && images !== null && images.length > 0)?{ uri: images[0] }: require('../assets/unilogo.png')} />
+          </View>
+          <View style={{ flex: 2, display: 'flex', flexDirection: 'column' }}>
+              <TouchableOpacity style={styles.button} onPress={goto}>
+                  <View style={{}}>
+                      <Text style={styles.name}>{eventName}</Text>
+                  </View>
+                  <View style={{ paddingTop: 15, paddingBottom: 16, }}>
+                      <Text style={styles.datetime}>{displayDateTime}</Text>
+                      <Text>{location.split(',')[0]}</Text>
+                  </View>
+              </TouchableOpacity>
+              <View style={styles.option}>
+                  <View style={{ flexDirection: 'column', fex: 2, display: 'flex' }}>
+                      <Text>{ageGroup} Years</Text>
+                      <Text>{participants} Participants</Text>
+                  </View>
+                  <View style={{ flex: 1, flexDirection: "row", justifyContent: 'space-around' }}>
+                      <Button icon={<FontAwesome name='edit' size={20} color="green" />} onPress={editEvent} type="clear" />
+                      <Button icon={<FontAwesome name='trash' size={20} />} onPress={deleteEvent} type="clear" />
+                  </View>
+              </View>
+          </View>
+      </View>
+  );
 }
 
 const styles = StyleSheet.create({
